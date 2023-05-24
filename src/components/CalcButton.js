ddf9d58e121ae7743c765dbdc3fd45766d3c1b6e
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-05-24 07:40:09 -0700

    added comments

--- src/components/CalcButton.js
+++ src/components/CalcButton.js
@@ -4,15 +4,20 @@ const CalcButton = (props) => {
 //functions
 const add = param => param + 1
 
-const switchCase = (param, num) => {
+const switchCase = (param, num) => {  // syncronous only
     switch (param) {
         case "+Add+":
+            //invoke a separate function 
             return add(props.num)
+
         case "-Subtract-":
+            // runs inside case
             return props.num - 1
 
         case "-Divide-":
+            // all params passed into switch case
             return num / 2
+            
         default:
             break;
     }
